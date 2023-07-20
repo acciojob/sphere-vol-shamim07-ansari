c0101bd71ddfe3d8f880e1474001859eed3f2aad
shamim07-ansari <124763751+shamim07-ansari@users.noreply.github.com>
2023-07-20 20:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
-    //Write your code here
-  
+    var radius = document.getElementById('radius').value;
+    radius = Math.abs(radius);
+    var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volume = volume.toFixed(4);
+    document.getElementById('volume').value = volume;
+    return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
